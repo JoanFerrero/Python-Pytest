a6bc325f578c2486b24e90bb3bb67bf4942ec409
unknown <joan.ferrero.montiel@gmail.com>
2023-08-23 17:06:19 +0200

    add new exercices

--- src/main.py
+++ src/main.py
@@ -16,3 +16,40 @@ def cambio(articulo, dinero_entregado):
         return articulo - dinero_entregado
     else:
         return 0
+
+
+def holaMundo():
+    return "¡Hola, mundo!"
+
+def numPI(num1, num2):
+    pares = []
+    impares = []
+    for i in range(num1, num2 + 1):
+        if i % 2 != 0:
+            impares.append(i)
+        else:
+            pares.append(i)
+    return [impares, pares]
+
+
+def countLengt(frase):
+    palabras = frase.split()
+    cantidad_palabras = len(palabras)
+    return cantidad_palabras
+
+
+def numPrimos(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if (num % i) == 0:
+            return False
+    return True
+
+
+def genFibonacci(n):
+    serie = [0, 1]
+    while len(serie) < n:
+        siguiente = serie[-1] + serie[-2]
+        serie.append(siguiente)
+    return serie
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,6 +1,6 @@
 import pytest
-from src.main import sum, login, cambio
-
+from src.main import sum, login, cambio, holaMundo, numPI, countLengt, numPrimos, genFibonacci
+# python -m pytest -v
 
 @pytest.mark.parametrize(
     "input_x, input_y, expected",
@@ -40,3 +40,45 @@ def test_cambio_params_pass(input_x, input_y, expected):
 
 def test_cambio_params_fail():
     assert not cambio(5, 5) == 1
+
+
+def test_holaMundo():
+    assert holaMundo() == "¡Hola, mundo!"
+
+
+@pytest.mark.parametrize(
+    "input_x, input_y, expected",
+    [
+        (1, 10, [[1,3,5,7,9], [2,4,6,8,10]]),
+        (1, 4, [[1,3], [2,4]])
+    ]
+)
+def test_numIP(input_x, input_y, expected):
+    assert numPI(input_x, input_y) == expected
+
+
+@pytest.mark.parametrize(
+    "input_x, expected",
+    [
+        ("Hola que tal.", 3),
+        ("Hola.", 1)
+    ]
+)
+def test_count(input_x, expected):
+    assert countLengt(input_x) == expected
+
+
+@pytest.mark.parametrize(
+    "input_x, expected",
+    [
+        (11, True),
+        (8, False),
+        (1, False)
+    ]
+)
+def test_numPrimos(input_x, expected):
+    assert numPrimos(input_x) == expected
+
+
+def test_fibonacci():
+    assert genFibonacci(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
